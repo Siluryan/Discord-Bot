7151c99b0e1b79a3db5919728c4be70e06100d1f
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2022-03-25 14:47:16 -0300

    Create DiscordBot.js

---
+++ DiscordBot.js
@@ -0,0 +1,81 @@
+const Discord = require("discord.js")
+const fetch = require("node-fetch")
+const keepAlive = require("./server")
+const Database = require("@replit/database")
+
+const db = new Database()
+const client = new Discord.Client()
+
+const Words = [""]
+
+const starterReturn = [
+  ""
+]
+
+db.get("return").then(returns => {
+  if (!returns || returns.length < 1) {
+    db.set("return", starterReturn)
+  }
+})
+
+db.get("responding").then(value => {
+  if (value == null) {
+    db.set("responding", true)
+  }
+})
+
+function updateReturn(returnMessage) {
+  db.get("return").then(returns => {
+    encouragements.push([returns])
+    db.set("return", returns)
+  })
+}
+
+function deleteReturn(index) {
+  db.get("return").then(returns => {
+    if (returns.length > index) {
+      returns.splice(index, 1)
+      db.set("return", returns)
+    }
+  })  
+}
+
+client.on("ready", () => {
+  console.log('Logged in ${client.user.tag}!')
+})
+
+client.on("message", msg => {
+
+  if (msg.content.startsWith("$new")) {
+    returnMessage = msg.content.split("$new ")[1]
+    updateReturn(returnMessage)
+    msg.channel.send("New return message added.")
+  }
+
+  if (msg.content.startsWith("$del")) {
+    index = parseInt(msg.content.split("$del ")[1])
+    deleteReturn(index)
+    msg.channel.send("return message deleted.")
+  }
+
+  if (msg.content.startsWith("$list")) {
+    db.get("return").then(returns => {
+      msg.channel.send(encouragements)
+    })
+  }
+
+  if (msg.content.startsWith("$responding")) {
+    value = msg.content.split("$responding ")[1]
+
+    if (value.toLowerCase() == "true") {
+      db.set("responding", true)
+      msg.channel.send("Responding is on.")
+    } else {
+       db.set("responding", false)
+      msg.channel.send("Responding is off.")     
+    }
+  }
+})
+
+keepAlive()
+client.login(process.env.TOKEN)
